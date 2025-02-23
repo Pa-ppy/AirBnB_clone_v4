10bf0ffa1d6e3dfae915b5a54b6f7089ad2488ea
Pa-ppy <rayjacks07@gmail.com>
2025-02-23 17:54:41 +0000

    Scripts

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,25 @@
+/* global $ */
+$(document).ready(function () {
+  const checkedAmenities = {};
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+
+  $('.amenities input[type="checkbox"]').change(function () {
+    const amenityId = $(this).attr('data-id');
+    const amenityName = $(this).attr('data-name');
+
+    if (this.checked) {
+      checkedAmenities[amenityId] = amenityName;
+    } else {
+      delete checkedAmenities[amenityId];
+    }
+
+    $('.amenities h4').text(Object.values(checkedAmenities).join(', '));
+  });
+});
